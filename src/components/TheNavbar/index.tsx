b64e5c1a6ac7f1a74e816a5800f93f1df0d8dfea
Kien Nguyen <kc97ble@gmail.com>
2022-07-04 22:39:45 +0700

    Refine TheNavbar

--- src/components/TheNavbar/index.tsx
+++ src/components/TheNavbar/index.tsx
@@ -1,24 +1,40 @@
 import * as React from "react";
 import { Button, Container, Image, Nav, Navbar } from "react-bootstrap";
+
 import imgLogoEcomap04 from "./assets/imgLogoEcomap04.png";
 
 export default function TheNavbar() {
   return (
-    <Navbar bg="$white" variant="light">
+    <Navbar bg="$white" variant="light" expand="lg">
       <Container>
         <Navbar.Brand href="#home">
           <Image src={imgLogoEcomap04} width="260px" />
         </Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Trang chủ</Nav.Link>
-          <Nav.Link href="#map">Bản đồ</Nav.Link>
-          <Nav.Link href="#education">Đào tạo</Nav.Link>
-          <Nav.Link href="#news">Tin tức</Nav.Link>
-        </Nav>
-        <Button className="me-1" variant="outline-secondary">
-          Đăng nhập
-        </Button>
-        <Button variant="primary">Đăng ký</Button>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" role="navigation">
+          <Nav className="me-auto">
+            <Nav.Link className="text-center" href="#home">
+              Trang chủ
+            </Nav.Link>
+            <Nav.Link className="text-center" href="#map">
+              Bản đồ
+            </Nav.Link>
+            <Nav.Link className="text-center" href="#education">
+              Đào tạo
+            </Nav.Link>
+            <Nav.Link className="text-center" href="#news">
+              Tin tức
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Item className="m-1" as={Button} variant="outline-secondary">
+              Đăng nhập
+            </Nav.Item>
+            <Nav.Item className="m-1" as={Button} variant="primary">
+              Đăng ký
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
